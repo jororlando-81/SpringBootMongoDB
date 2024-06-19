e7904d6a20df155d3fbb10482309f98eb65c2fbd
“Jorge <Jorgeorlando81@hotmail.com>
2024-06-19 10:28:12 -0300

    Added comments on posts

--- src/main/java/com/project/springBootMongoDB/config/Instantiation.java
+++ src/main/java/com/project/springBootMongoDB/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.project.springBootMongoDB.domain.Post;
 import com.project.springBootMongoDB.domain.User;
 import com.project.springBootMongoDB.dto.AuthorDTO;
+import com.project.springBootMongoDB.dto.CommentDTO;
 import com.project.springBootMongoDB.repository.PostRepository;
 import com.project.springBootMongoDB.repository.UserRepository;
 
@@ -40,6 +41,14 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("10/06/2024"), "Gym", "I´m going", new AuthorDTO(marcos) ) ;
 		Post post2 = new Post(null, sdf.parse("11/06/2024"), "Good night", "I'm going to bed", new AuthorDTO(marcos) ) ;
+		
+		CommentDTO comment1 = new CommentDTO( "Me too" , sdf.parse("10/06/2024"), new AuthorDTO(betina) ) ;
+		CommentDTO comment2 = new CommentDTO( "I'll be a few minutes late" , sdf.parse("10/06/2024"), new AuthorDTO(braian) ) ;
+		CommentDTO comment3 = new CommentDTO( "Sleep tight, see you in the morning!" , sdf.parse("11/06/2024"), new AuthorDTO(betina) ) ;
+		
+		post1.getComments().addAll( Arrays.asList( comment1  , comment2 ));	
+		post2.getComments().addAll( Arrays.asList( comment3 ) )  ;
+		
 		
 		postRepository.saveAll(Arrays.asList(post1, post2) );
 		
