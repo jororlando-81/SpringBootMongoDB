20385f431992ef984a2520d6a885529a0e802499
“Jorge <Jorgeorlando81@hotmail.com>
2024-06-21 12:09:54 -0300

    Find by title with @Query

--- src/main/java/com/project/springBootMongoDB/repository/PostRepository.java
+++ src/main/java/com/project/springBootMongoDB/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.project.springBootMongoDB.repository;
 
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.project.springBootMongoDB.domain.Post;
 
@@ -9,6 +10,9 @@ import com.project.springBootMongoDB.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post , String>   {
+	
+	@Query( "{ 'title' : { $regex:?0 , $options: 'i' } }" )
+	List<Post> searchTitle( String text ) ;
 	
 	List<Post> findByTitleContainingIgnoreCase( String text ) ;
 
